c951c1341c9ade8a5a9f27052012fe50b0b20b81
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2021-09-18 15:19:51 +0900

    ツムを選択して消せるように

--- Assets/App/Scripts/Presenters/TsumuRootPresenter.cs
+++ Assets/App/Scripts/Presenters/TsumuRootPresenter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using App.Lib;
 using App.MasterData;
-using App.Types;
 using App.Views;
 using Cysharp.Threading.Tasks;
 using UniRx;
@@ -16,8 +15,11 @@ namespace App.Presenters
     {
         private MainRootView _mainRootView;
         private readonly int _maxTsumuCount = 40;
+        private readonly int _canSelectDistance = 300;
 
-        private List<TsumuData> _tsumuDatas = new List<TsumuData>();
+        private List<TsumuData> _tsumuDataList = new List<TsumuData>();
+        private List<TsumuView> _selectingTsumuViewList = new List<TsumuView>();
+        private List<TsumuView> _tsumuViewList = new List<TsumuView>();
 
         public TsumuRootPresenter(MainRootView view)
         {
@@ -31,7 +33,7 @@ namespace App.Presenters
         public async void Initialize()
         {
             SetEvents();
-            _tsumuDatas = LoadTsumuData();
+            _tsumuDataList = LoadTsumuData();
             for (var i = 0; i < 10; i++)
             {
                 await SpawnTsumuAsync();
@@ -45,13 +47,102 @@ namespace App.Presenters
 
         private async UniTask SpawnTsumuAsync()
         {
-            var data = _tsumuDatas.OrderBy(x => Guid.NewGuid()).First();
+            var data = _tsumuDataList.OrderBy(x => Guid.NewGuid()).First();
             Assert.IsNotNull(data);
             var tsumuView = await CreateViewAsync<TsumuView>();
             _mainRootView.SetParentTsumu(tsumuView);
             tsumuView.Initialize(data, new Vector3(0, 0, 0));
-            tsumuView.OnClickAsObservable.Subscribe(x => tsumuView.ChangeColor(Color.red));
+            tsumuView.OnPointerEnterAsObservable.Subscribe(OnPointerEntertsumu);
+            tsumuView.OnPointerDownAsObservable.Subscribe(OnPointerDownTsumu);
+            tsumuView.OnPointerUpAsObservable.Subscribe(x => OnPointerUpTsumu(x).Forget());
+            
             await UniTask.Delay(500);
+        }
+
+        private void SelectTsumu(TsumuView view)
+        {
+            _selectingTsumuViewList.Add(view);
+            view.ChangeColor(true);
+        }
+
+
+        private void UnSelectTsumuAll()
+        {
+            _selectingTsumuViewList.ForEach(x => x.ChangeColor(false));
+            _selectingTsumuViewList.Clear();
+        }
+
+        private void OnPointerDownTsumu(TsumuView view)
+        {
+            if (_selectingTsumuViewList.Count > 0)
+            {
+                return;
+            }
+
+            SelectTsumu(view);
+        }
+
+        private UniTask OnPointerUpTsumu(TsumuView view)
+        {
+            if (_selectingTsumuViewList.Count < 3)
+            {
+                UnSelectTsumuAll();
+                return UniTask.CompletedTask;
+            }
+
+            DespawnSelectingTsumusAsync().Forget();
+            UnSelectTsumuAll();
+            return UniTask.CompletedTask;
+        }
+
+        private async UniTask DespawnSelectingTsumusAsync()
+        {
+            var chain = _selectingTsumuViewList.Count;
+            var animatingTsumuViews = _selectingTsumuViewList.ToArray();
+            foreach (var view in animatingTsumuViews)
+            {
+                await DespawnTsumuAsync(view);
+            }
+        }
+
+        private async UniTask DespawnTsumuAsync(TsumuView tsumuView)
+        {
+            _tsumuViewList.Remove(tsumuView);
+            await tsumuView.CloseAsync();
+        }
+
+        private void OnPointerEntertsumu(TsumuView view)
+        {
+            if (!CanSelect(view))
+            {
+                return;
+            }
+
+            SelectTsumu(view);
+        }
+
+        private bool CanSelect(TsumuView view)
+        {
+            if (_selectingTsumuViewList.Count == 0)
+            {
+                return false;
+            }
+
+            var lastView = _selectingTsumuViewList.Last();
+
+            if (lastView.TsumuType != view.TsumuType)
+            {
+                return false;
+            }
+
+            var difference = lastView.GetPosition() - view.GetPosition();
+            Debug.Log(difference);
+            if (Mathf.Abs(difference.x) < _canSelectDistance && Mathf.Abs(difference.y) < _canSelectDistance)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- Assets/App/Scripts/Views/TsumuView.cs
+++ Assets/App/Scripts/Views/TsumuView.cs
@@ -4,6 +4,7 @@ using App.MasterData;
 using App.Types;
 using Cysharp.Threading.Tasks;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
@@ -16,18 +17,28 @@ namespace App.Views
         [SerializeField] private Button _button;
 
         private TsumuData _tsumuData;
+        private readonly Subject<TsumuView> _onPointerDownSubject = new Subject<TsumuView>();
+        private readonly Subject<TsumuView> _onPointerEnterSubject = new Subject<TsumuView>();
+        private readonly Subject<TsumuView> _onPointerUpSubject = new Subject<TsumuView>();
 
-        public IObservable<Unit> OnClickAsObservable => _button.OnClickAsObservable().TakeUntilDestroy(this);
+        public IObservable<TsumuView> OnPointerUpAsObservable => _onPointerUpSubject.TakeUntilDestroy(this);
+        public IObservable<TsumuView> OnPointerEnterAsObservable => _onPointerEnterSubject.TakeUntilDestroy(this);
+        public IObservable<TsumuView> OnPointerDownAsObservable => _onPointerDownSubject.TakeUntilDestroy(this);
 
+        public TsumuType TsumuType => _tsumuData.TsumuType;
+
+        
         protected override UniTask OnLoadAsync()
         {
-            
+            _button.OnPointerEnterAsObservable().Subscribe(x => _onPointerEnterSubject.OnNext(this)).AddTo(this);
+            _button.OnPointerUpAsObservable().Subscribe(x => _onPointerUpSubject.OnNext(this)).AddTo(this);
+            _button.OnPointerDownAsObservable().Subscribe(x => _onPointerDownSubject.OnNext(this)).AddTo(this);
             return base.OnLoadAsync();
         }
 
-        public void ChangeColor(Color color)
+        public void ChangeColor(bool state)
         {
-            _button.image.color = color;
+            _button.image.color = state ? Color.black : TsumuColor.ConvertTsumuColor(TsumuType);
         }
 
         public void Initialize(TsumuData data, Vector3 position)
@@ -35,6 +46,17 @@ namespace App.Views
             _tsumuData = data;
             _button.image.color = TsumuColor.ConvertTsumuColor(data.TsumuType);
             transform.localPosition = position;
+        }
+
+        public UniTask CloseAsync()
+        {
+            Dispose();
+            return UniTask.CompletedTask;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return transform.localPosition;
         }
     }
 }
