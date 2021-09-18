28d04cb662df52d83d813f694e26239a24396a64
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2021-09-18 13:33:08 +0900

    ツムを生成してクリックすると色が変わるように

--- Assets/App/Scripts/Presenters/TsumuRootPresenter.cs
+++ Assets/App/Scripts/Presenters/TsumuRootPresenter.cs
@@ -1,21 +1,57 @@
-﻿using App.Lib;
-using App.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using App.Lib;
+using App.MasterData;
+using App.Types;
 using App.Views;
+using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace App.Presenters
 {
     public sealed class TsumuRootPresenter : RootPresenterBase
     {
-        private TsumuRootView _tsumuRootView;
-        public TsumuRootPresenter(TsumuRootView view)
+        private MainRootView _mainRootView;
+        private readonly int _maxTsumuCount = 40;
+
+        private List<TsumuData> _tsumuDatas = new List<TsumuData>();
+
+        public TsumuRootPresenter(MainRootView view)
         {
-            _tsumuRootView = view;
+            _mainRootView = view;
         }
 
-        public void Initialize()
+        private void SetEvents()
         {
-            Debug.Log($"GameModelInstance is {GameModel.Instance != null}");
+        }
+
+        public async void Initialize()
+        {
+            SetEvents();
+            _tsumuDatas = LoadTsumuData();
+            for (var i = 0; i < 10; i++)
+            {
+                await SpawnTsumuAsync();
+            }
+        }
+
+        private List<TsumuData> LoadTsumuData()
+        {
+            return Resources.LoadAll<TsumuData>("MasterData/").ToList();
+        }
+
+        private async UniTask SpawnTsumuAsync()
+        {
+            var data = _tsumuDatas.OrderBy(x => Guid.NewGuid()).First();
+            Assert.IsNotNull(data);
+            var tsumuView = await CreateViewAsync<TsumuView>();
+            _mainRootView.SetParentTsumu(tsumuView);
+            tsumuView.Initialize(data, new Vector3(0, 0, 0));
+            tsumuView.OnClickAsObservable.Subscribe(x => tsumuView.ChangeColor(Color.red));
+            await UniTask.Delay(500);
         }
     }
 }
---
+++ Assets/App/Scripts/Views/TsumuView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using App.Lib;
+using App.MasterData;
+using App.Types;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+
+namespace App.Views
+{
+    [PrefabPath("Prefabs/TsumuView")]
+    public class TsumuView : ViewBase
+    {
+        [SerializeField] private Button _button;
+
+        private TsumuData _tsumuData;
+
+        public IObservable<Unit> OnClickAsObservable => _button.OnClickAsObservable().TakeUntilDestroy(this);
+
+        protected override UniTask OnLoadAsync()
+        {
+            
+            return base.OnLoadAsync();
+        }
+
+        public void ChangeColor(Color color)
+        {
+            _button.image.color = color;
+        }
+
+        public void Initialize(TsumuData data, Vector3 position)
+        {
+            _tsumuData = data;
+            _button.image.color = TsumuColor.ConvertTsumuColor(data.TsumuType);
+            transform.localPosition = position;
+        }
+    }
+}
