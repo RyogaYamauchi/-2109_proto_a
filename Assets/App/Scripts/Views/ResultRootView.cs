2f433ff7e2dba2515b3d97b855f7620f177017c5
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2021-09-20 13:08:46 +0900

    レートの実装

--- Assets/App/Scripts/Views/ResultRootView.cs
+++ Assets/App/Scripts/Views/ResultRootView.cs
@@ -68,6 +68,8 @@ namespace App.Views
                 myRoom.IsOpen = false;            // 部屋を閉じる
                 myRoom.IsVisible = false;         // ロビーから見えなくする
             }
+
+            SetRate(winOrLose);
             
             PhotonNetwork.LeaveRoom();
             PhotonNetwork.Disconnect();
@@ -86,6 +88,27 @@ namespace App.Views
                 audioSource.PlayOneShot(LoseSound);
             }
 
+        }
+
+        private void SetRate(bool isWin)
+        {
+            var range = 50;
+            var rate = 0;
+            if (PlayerPrefs.HasKey("rate"))
+            {
+                rate = PlayerPrefs.GetInt("rate");
+                rate += isWin ? range : -range;
+                
+            }
+            else
+            {
+                rate = 1000;
+            }
+            
+            PlayerPrefs.SetInt("rate", rate);
+
+            var plusOrMinul = isWin ? "+" : "-";
+            _rateltText.text = $"rate : {rate}\n{plusOrMinul}{range}...";
         }
         
     }
